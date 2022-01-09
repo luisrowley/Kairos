daf85797c01f2031fa0e6598bad1b15ab17eaf51
Luis De Benito <louisdebenito@gmail.com>
2022-01-09 18:52:37 +0100

    removed positional argument error

--- utils.py
+++ utils.py
@@ -2,7 +2,7 @@
 Wordlist is expected as simple list only containing userid,
 password is mocked as we are not interested in its value at this point
 """
-def unpack(self, fline):
+def unpack(fline):
     userid = fline
     passwd = 'foobar'
     return userid, passwd
